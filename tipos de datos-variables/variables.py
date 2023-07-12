55b4184707800000b87e382fed54743d0b6d3ff4
danipozfez <danipozfez@gmail.com>
2023-07-12 19:02:23 +0200

    creando variables

---
+++ tipos de datos-variables/variables.py
@@ -0,0 +1,10 @@
+nombre = "daniel"
+print(nombre)
+nombre = "Tony Soprano"
+edad = 51
+nombre = "Julia"
+apellido = "Roberts"
+nombreCompleto = nombre + " " + apellido
+print(nombreCompleto)
+curso = "Python"
+print("Estás tomando un curso de " + curso)
